59570a007142664e0a4138b613bba712e183dd35
daniellea <daniellea@jfrog.com>
2025-04-29 15:13:45 +0300

    MAR-9833 - Research add new gtm

--- src/main.js
+++ src/main.js
@@ -25,6 +25,49 @@ export default function(Vue, { router, head, isClient }) {
     content: "default-src *  'self' 'unsafe-eval' 'unsafe-inline'  https://jfrog.com; img-src 'self' * data: ; font-src 'self' * data: ;"
   });
 
+  head.script.push({
+    innerHTML: `
+    <script
+      data-cfasync="false"
+      data-tracker-overrides="GoogleConsentMode:security_storage=on;ad_storage=SaleOfInfo,Advertising;ad_user_data=SaleOfInfo,Advertising;ad_personalization=SaleOfInfo,Advertising;analytics_storage=Analytics,SaleOfInfo;functionality_storage=Functional,SaleOfInfo;personalization_storage=Functional,SaleOfInfo"
+      src="https://transcend-cdn.com/cm/f0071674-c641-4cf3-9d31-303ec0c86b1b/airgap.js"
+      data-languages="en"
+    ></script>
+  `,
+    type: "text/javascript",
+    charset: "utf-8"
+  });
+
+  head.script.push({
+    innerHTML: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "GTM-T6MF8M");
+      gtag('set', 'developer_id.dODQ2Mj', true);
+    `,
+    type: "text/javascript",
+    charset: "utf-8"
+  });
+
+  head.script.push({
+    innerHTML: `
+    if (window.airgap) {
+      const cookieSettings = document.getElementById('cookie_settings') || document.querySelector('.ot-sdk-show-settings a');
+      cookieSettings.href = "#";
+      cookieSettings.target = "_self";
+      cookieSettings.onclick = () => {
+        transcend.showConsentManager({ viewState: 'CompleteOptions' });
+      };
+    }
+  `,
+    type: "text/javascript",
+    charset: "utf-8",
+    body: true
+  });
+
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
 
